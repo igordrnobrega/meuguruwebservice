1887aed4789381563ba236f184e9ef6243d3c8aa
Igor Nóbrega <igordouradonobrega@gmail.com>
2015-02-03 22:32:06 -0200

    Initial e EventSearch

--- src/controllers.php
+++ src/controllers.php
@@ -9,15 +9,3 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 // API
 $app->get('/', 'App\\Controllers\\Api::initialAction');
 $app->get('/event/search', 'App\\Controllers\\Api::eventSearchAction');
-
-
-
-// $app->get('/jogar', 'App\\Controllers\\Index::indexAction');
-// $app->post('/signin', 'App\\Controllers\\Index::signinAction');
-// $app->get('/signin', 'App\\Controllers\\Index::signinAction');
-// $app->get('/etapa/{hash}', 'App\\Controllers\\Index::etapaAction');
-// $app->post('/resp', 'App\\Controllers\\Index::respostaAction');
-
-// // BACKEND
-// $app->get('/4c91d222b139eeccd6eb342f512ec180', 'App\\Controllers\\Backend::indexAction');
-// $app->get('/retirado', 'App\\Controllers\\Backend::retiraAction');
