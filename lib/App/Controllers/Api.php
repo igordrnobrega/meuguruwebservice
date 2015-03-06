4dacacda40b7e48bf6c981fe06f81f8954f07bd5
Igor Nóbrega <igordouradonobrega@gmail.com>
2015-03-06 03:45:59 -0300

    Ajuste getFornecedores

--- lib/App/Controllers/Api.php
+++ lib/App/Controllers/Api.php
@@ -316,7 +316,7 @@ class Api {
             'inner join imp_terms segmento on segmento.term_id = itt.term_id ' .
             'inner join imp_postmeta detalhes on detalhes.post_id = evento.ID ' .
             'and detalhes.meta_value != "" ' .
-            'and tt.taxonomy = "fornecedores" ';
+            'and itt.taxonomy = "fornecedores" ';
 
         try {
             $sqlResult = $app['db']->fetchAll($sql);
