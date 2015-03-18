2355d085f0c9774fe9d348b7f28b7f4e07238eaa
Igor Nóbrega <igordouradonobrega@gmail.com>
2015-03-17 23:42:36 -0300

    Noticia mudanca de query

--- lib/App/Controllers/Api.php
+++ lib/App/Controllers/Api.php
@@ -513,9 +513,12 @@ class Api {
             foreach ($sqlResult as $key => $value) {
                 $meta_key = explode('/-/', $value['meta_key']);
                 $meta_value = explode('/-/', $value['meta_value']);
-                foreach ($meta_key as $keyM => $valueM) {
-                    $sqlResult[$key][$valueM] = $meta_value[$key];
-                }
+
+                echo sizeof($meta_key);
+                echo sizeof($meta_value);
+                // foreach ($meta_key as $keyM => $valueM) {
+                //     $sqlResult[$key][$valueM] = $meta_value[$key];
+                // }
             }
 
         } catch (\PDOException $e) {
