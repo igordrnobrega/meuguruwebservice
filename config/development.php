a0f91ade48ca4face13977d24e8f48693673526f
Igor Nóbrega <igordouradonobrega@gmail.com>
2015-06-10 22:15:54 -0300

    Config de desen

--- config/development.php
+++ config/development.php
@@ -14,10 +14,10 @@ $app['debug'] = true;
 $app['config'] = array(
     'database' => array(
         'driver'   => 'pdo_mysql',
-        'dbname'   => 'meuguru_web',
+        'dbname'   => 'meuguru_dev',
         'host'     => '177.54.144.49',
-        'user'     => 'meuguru_abril',
-        'password' => 'Nova2015Guru20',
+        'user'     => 'meuguru_guru',
+        'password' => 'GURU@K2@mySQL',
         'charset'  => 'UTF8',
         'port'     => 3306
     )
